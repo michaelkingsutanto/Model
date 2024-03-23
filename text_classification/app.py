f994bedc8f4fd146dadc19228af88817803c037c
michaelks <michaelkingsutanto@gmail.com>
2024-03-23 20:59:18 +0800

    Add streamlit app for project 3 text classification

---
+++ text_classification/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+import pickle
+import pandas as pd
+
+# Load your model
+with open('text_classification/model_classification.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Title
+st.title("Text Classification with Naive Bayes")
+
+# User text input
+user_input = st.text_area("Enter text here")
+
+if st.button('Predict'):
+    prediction = model.predict([user_input])[0]
+    if prediction == 0:
+        st.write(Input doesn't align with Omega community")
+    else:
+        st.write(Input aligns with Omega community")
+
